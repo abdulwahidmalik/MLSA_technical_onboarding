8832686a3972184f98025e498c17c2116409ca6d
Abdul Wahid <abdulwahid9274@gmail.com>
2023-12-13 04:17:45 +0500

    check

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/spidy.jpg";
+import image from "../../images/spidy.jpg";
 
 const imageAltText = " A Spider Net abstract background";
 
