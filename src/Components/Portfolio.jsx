341cb644efb84606fc9a9ed8ff577979a9f315af
Abdul Wahid <abdulwahid9274@gmail.com>
2023-12-13 04:22:43 +0500

    check 2

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/student.jpg";
+import image from "../../images/student.jpg";
 
 const imageAltText = "dest image of a desk with a laptop, notebook, and coffee";
 
